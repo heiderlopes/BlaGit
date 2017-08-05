fd266348c6b34a7ba13328760cac19de9ad14fe7
logonpf <heidetreinamentos@gmail.com>
2017-08-05 17:09:10 -0300

    Instancia do botao

--- app/src/main/java/br/com/heiderlopes/blagit/MainActivity.java
+++ app/src/main/java/br/com/heiderlopes/blagit/MainActivity.java
@@ -2,12 +2,16 @@ package br.com.heiderlopes.blagit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btClicaAqui;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btClicaAqui = (Button)findViewById(R.id.btClicaAqui);
     }
 }
